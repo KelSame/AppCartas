7eedad10944618bbbb85097fdb94ebe7507915c8
KelSame <skelsame@gmail.com>
2025-06-08 18:55:49 -0500

    Delete modelos directory

--- modelos/sobre.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const esquemaSobre = new mongoose.Schema({
-  usuarioId: { type: Number, required: true, unique: true },
-  sinAbrir: { type: Number, default: 0 },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Sobre', esquemaSobre);
