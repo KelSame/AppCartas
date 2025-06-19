9366d5c53ef35fb2c87eee513b1caab127e54c64
KelSame <skelsame@gmail.com>
2025-06-19 16:54:25 -0500

    Delete modelos directory

--- modelos/cartasUsuarios.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const cartaUsuarioSchema = new mongoose.Schema({
-  usuarioId: Number,
-  cartaId: mongoose.Schema.Types.ObjectId,
-  alias: String,
-  cantidad: Number,
-});
-module.exports = mongoose.model('CartaUsuario', cartaUsuarioSchema);
