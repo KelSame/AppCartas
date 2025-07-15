574eeed71054ce2bd2bcd81c58b393ec2da57578
KelSame <skelsame@gmail.com>
2025-07-14 19:14:33 -0500

    Delete modelos directory

--- modelos/historialCompras.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const compraSchema = new mongoose.Schema({
-  usuarioId: Number,
-  tipo: String,
-  precio: Number,
-  fecha: Date,
-});
-module.exports = mongoose.model('HistorialCompra', compraSchema);
--- modelos/sobresUsuarios.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const sobreUsuarioSchema = new mongoose.Schema({
-  usuarioId: Number,
-  basicos: { type: Number, default: 0 },
-  raros: { type: Number, default: 0 },
-  elites: { type: Number, default: 0 },
-});
-module.exports = mongoose.model('SobreUsuario', sobreUsuarioSchema);
